0034fd917098a9dd4d78dd3fe2fd094141756e1b
Oreno <kanekosho@Kanekos-MBP-4.attlocal.net>
2024-10-02 00:11:39 -0700

    Finish 5. Using Structs to Structure Related Data

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -8,6 +8,24 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    //The Self keyword in ther return type and in the body of the function are aliases for the type
+    //that appears after the impl keyword.
+    //In this case, Self is Rectangle
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
 }
 
 fn main() {
@@ -15,9 +33,31 @@ fn main() {
         width: 30,
         height: 50,
     };
+    let rect2 = Rectangle{
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle{
+        width: 60,
+        height: 45,
+    };
+    let sq1 = Rectangle::square(3);
 
     println!(
         "The area of the rectangle is {} square pixels.",
         rect1.area()
     );
+
+    if rect1.width(){
+        println!(
+        "The rectangle has a nonzero width; it is {}", rect1.width
+        );
+    }
+        
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    println!("sq1 : {sq1:?}");
+
+
 }
