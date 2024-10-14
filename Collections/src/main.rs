2b38449696332eace26d81d789c20a16477287ec
Oreno <kanekosho@Kanekos-MacBook-Pro-4.local>
2024-10-14 14:36:34 -0700

    Finidh 8. Common Collection

--- Collections/src/main.rs
+++ Collections/src/main.rs
@@ -77,6 +77,36 @@ fn main() {
     for b in "Зд".bytes() {
         println!("{b}");
     }
-    
 
+    use std::collections::HashMap;
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    for (key, value) in &scores {
+        println!("{key} : {value}");
+    }
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+    println!("score : {score}");
+
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+    // field_name and field_value are invalid at this point, try using them and
+    // see what compiler error you get!
+    //error[E0382]: borrow of moved value: `field_name`
+    //println!("field_name: {field_name}");
+
+
+    let text = "hello world wonderful world";
+    let mut map = HashMap::new();
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+    println!("{map:?}")
 }
